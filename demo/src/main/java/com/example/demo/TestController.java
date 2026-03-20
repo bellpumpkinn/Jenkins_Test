0d38bbd49e06696b309103096aa0eae5574fd82b
jhpark <hojong504@gmail.com>
2026-03-20 14:40:36 +0900

    77777로 수정

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/test")
 	public String test() {
-		return "66666";
+		return "77777";
 	}
 }
